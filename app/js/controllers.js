aedf41a210d7609694589aa41eeb796de802066f
Whitney O'Banner <wobanner@gmail.com>
2015-12-14 11:20:48 +0800

    Create containers to hold music data
    I.e. empty arrays for songs, artists, albums, and favorites

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,10 +6,14 @@ angular.module('myApp.controllers', [])
   .controller('HomeController', ['$scope', function($scope) {
   }])
   .controller('AlbumsController', ['$scope', function($scope) {
+    $scope.albums = [];
   }])
   .controller('ArtistsController', ['$scope', function($scope) {
+    $scope.artists = [];
   }])
   .controller('SongsController', ['$scope', function($scope) {
+    $scope.songs = [];
   }])
   .controller('FavoritesController', ['$scope', function($scope) {
+    $scope.favorites = [];
   }]);
