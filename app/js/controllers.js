8ce2ffbda942fe94a0d25dd3004eb5de3aa99cb5
Whitney O'Banner <wobanner@gmail.com>
2015-12-14 11:37:41 +0800

    Add container for metadata
    Metadata includes song, artist, and album info collected from form
    Might want to rethink how Favorites are handled

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,15 +4,20 @@
 
 angular.module('myApp.controllers', [])
   .controller('HomeController', ['$scope', function($scope) {
+    $scope.allMusic = [];
+    $scope.metadata = {song: '', artist: '', album: ''}; 
   }])
   .controller('AlbumsController', ['$scope', function($scope) {
     $scope.albums = [];
+    // Get last property of each metadata object and add to array
   }])
   .controller('ArtistsController', ['$scope', function($scope) {
     $scope.artists = [];
+    // Get second property of each metadata object and add to array
   }])
   .controller('SongsController', ['$scope', function($scope) {
     $scope.songs = [];
+    // Get first property of each metadata object and add to array
   }])
   .controller('FavoritesController', ['$scope', function($scope) {
     $scope.favorites = [];
