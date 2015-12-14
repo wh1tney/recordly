d9160c9bf8b87b1ce1fc95c20e473749127f54b4
Whitney O'Banner <wobanner@gmail.com>
2015-12-14 16:47:03 +0800

    Save user input! Yay!
    Restructure controllers to save music data in arrays
    Add save function to controller and ng-submit to form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,20 +5,27 @@
 angular.module('myApp.controllers', [])
   .controller('HomeController', ['$scope', function($scope) {
     $scope.allMusic = [];
+    $scope.albums = [];
+    $scope.artists = [];
+    $scope.songs = [];
+
     $scope.metadata = {song: '', artist: '', album: ''}; 
+
+    $scope.saveMusic = function() {
+      $scope.allMusic.push($scope.metadata);
+      $scope.albums.push($scope.metadata.album);
+      $scope.artists.push($scope.metadata.artist);
+      $scope.songs.push($scope.metadata.song);
+
+      // Clear out data after it has been saved!
+      $scope.metadata = {song: '', artist: '', album: ''}; 
+    };
   }])
   .controller('AlbumsController', ['$scope', function($scope) {
-    $scope.albums = [];
-    // Get last property of each metadata object and add to array
   }])
   .controller('ArtistsController', ['$scope', function($scope) {
-    $scope.artists = [];
-    // Get second property of each metadata object and add to array
   }])
   .controller('SongsController', ['$scope', function($scope) {
-    $scope.songs = [];
-    // Get first property of each metadata object and add to array
   }])
   .controller('FavoritesController', ['$scope', function($scope) {
-    $scope.favorites = [];
   }]);
